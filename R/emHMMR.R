0229e4167ecf4ce47c8426f36a668fa41f34b37f
Florian Lecocq <florian.lecocq@outlook.com>
2019-07-15 15:22:03 +0200

    Add 'URL' field in DESCRIPTION file
    - Update documentation;
    - Add 'URL' field in DESCRIPTION file;

--- R/emHMMR.R
+++ R/emHMMR.R
@@ -22,8 +22,9 @@
 #'   set at 3.
 #' @param variance_type Optional character indicating if the model is
 #'   "homoskedastic" or "heteroskedastic" (i.e same variance or different
-#'   variances for each of the K regmies). By default the model is "heteroskedastic".
-#'  
+#'   variances for each of the K regmies). By default the model is
+#'   "heteroskedastic".
+#'
 #' @param n_tries Optional. Number of runs of the EM algorithm. The solution
 #'   providing the highest log-likelihood will be returned.
 #'
@@ -42,101 +43,101 @@
 #' @seealso [ModelHMMR], [ParamHMMR], [StatHMMR]
 #' @export
 emHMMR <- function(X, Y, K, p = 3, variance_type = c("heteroskedastic", "homoskedastic"), n_tries = 1, max_iter = 1500, threshold = 1e-6, verbose = FALSE) {
-  
+
   nb_good_try <- 0
   total_nb_try <- 0
   best_loglik <- -Inf
-  
+
   while (nb_good_try < n_tries) {
-    
+
     if (n_tries > 1 && verbose) {
       cat(paste0("EM try number: ", nb_good_try + 1, "\n\n"))
     }
     total_nb_try <- total_nb_try + 1
-    
+
     ## EM Initializaiton step
     ## Initialization of the Markov chain parameters, the regression coefficients, and the variance(s)
     variance_type <- match.arg(variance_type)
     param <- ParamHMMR$new(X = X, Y = Y, K = K, p = p, variance_type = variance_type)
     param$initParam(nb_good_try + 1)
-    
+
     iter <- 0
     prev_loglik <- -Inf
     converged <- FALSE
     top <- 0
-    
+
     stat <- StatHMMR(paramHMMR = param)
-    
+
     while ((iter <= max_iter) && !converged) {
 
       # E step : calculate the tau_tk (p(Zt=k|y1...ym;theta)) and xi t_kl (and the log-likelihood) by
       #  forwards backwards (computes the alpha_tk and beta_tk)
       stat$EStep(param)
-      
+
       # M step
       param$MStep(stat)
-      
+
       # End of an EM iteration
-      
+
       iter <-  iter + 1
-      
+
       # Test of convergence
       lambda <- 1e-9 # If a bayesian prior on the beta's
       stat$loglik <- stat$loglik + log(lambda)
-      
+
       if (verbose) {
         cat(paste0("EM: Iteration : ", iter, " || log-likelihood : "  , stat$loglik, "\n"))
       }
-      
+
       if ((prev_loglik - stat$loglik) > 1e-4) {
         top <- top + 1
         if (top == 10) {
           warning(paste0("EM log-likelihood is decreasing from ", prev_loglik, "to ", stat$loglik, " !"))
         }
       }
-      
+
       converged <- (abs(stat$loglik - prev_loglik) / abs(prev_loglik) < threshold)
       if (is.na(converged)) {
         converged <- FALSE
       } # Basically for the first iteration when prev_loglik is Inf
-      
+
       prev_loglik <- stat$loglik
       stat$stored_loglik[iter] <- stat$loglik
-      
+
     } # End of the EM loop
-    
+
     if (n_tries > 1 && verbose) {
       cat(paste0("Max value of the log-likelihood: ", stat$loglik, "\n\n"))
     }
-    
+
     if (length(param$beta) != 0) {
       nb_good_try <- nb_good_try + 1
       total_nb_try <- 0
-      
+
       if (stat$loglik > best_loglik) {
-        
+
         statSolution <- stat$copy()
         paramSolution <- param$copy()
-        
+
         best_loglik <- stat$loglik
       }
     }
-    
+
     if (total_nb_try > 500) {
       stop(paste("can't obtain the requested number of classes"))
     }
-    
+
   }
-  
+
   if (n_tries > 1) {
     cat(paste0("Best value of the log-likelihood: ", statSolution$loglik, "\n"))
   }
-  
+
   # Smoothing state sequences : argmax(smoothing probs), and corresponding binary allocations partition
   statSolution$MAP()
-  
+
   # Finish the computation of statistics
   statSolution$computeStats(paramSolution)
-  
+
   return(ModelHMMR(param = paramSolution, stat = statSolution))
 }
